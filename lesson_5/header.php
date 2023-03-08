3674f6ed1fbf12d23507e6d666fe0eb0a3a1e12d
Hakimov <naim.nanocoding@gmail.com>
2023-03-08 16:11:49 +0500

    added lesson_5

---
+++ lesson_5/header.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+    <title>Lesson 2</title>
+</head>
+
+<style>
+
+    section {
+        max-width: 800px;
+        margin: 0 auto;
+        color: #444444;
+    }
+    * {
+        font-family: 'Roboto', sans-serif;
+    }
+
+    header {
+        height: 255px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin: 0 auto;
+        background-image: url("https://cc-prod.scene7.com/is/image/CCProdAuthor/Photography%20Page%20JDI_marquee_tb?$pjpeg$&jpegSize=200&wid=1199");
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+        color: white;
+    }
+
+    header h1 {
+        font-size: 35px;
+        line-height: 40px;
+    }
+    header p {
+        max-width: 300px;
+        text-align: center;
+    }
+
+    main {
+        margin: 150px auto 0 auto;
+        max-width: 600px;
+    }
+
+    footer {
+        margin-top: 40px;
+        display: flex;
+        column-gap: 40px;
+        align-items: center;
+        justify-content: center;
+        padding: 0 15px;
+        background: #847462;
+        color: white;
+    }
+
+</style>
+<body>
